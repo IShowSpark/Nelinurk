04847795fad64830b510a2ce1091b30721702984
opilane <opilane@HP-CZC6297BB7>
2022-03-02 14:15:21 +0200

    Add project files.

---
+++ Nelinurk.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Nelinurk
+{
+    class Nelinurk
+    {
+        string tüüp;
+        int status;
+        double kõrgus;
+        double laius;
+        public Nelinurk() {}
+        public Nelinurk(double Kõrgus,double Laius)
+        {
+            kõrgus = Kõrgus;
+            laius = Laius;
+        }
+        public string Tüüp
+        {
+            get
+            {
+                if (kõrgus==laius || status!=90)
+                {
+                    tüüp = "Romb";
+                }
+                else if (kõrgus==laius || status == 90)
+                {
+                    tüüp = "Ruut";
+                }
+                else
+                {
+                    tüüp = "ristkülik";
+                }
+                return tüüp;
+            }
+        }
+        public int Status
+        {
+            get { return status; }
+            set { }
+        }
+        public double Pindala()
+        {
+            double S;
+            S = kõrgus * laius;
+            return Pindala;
+        }
+        public double Perimeeter()
+        {
+            double P;
+            P = (kõrgus + laius) * 2;
+            return Perimeeter;
+        }
+        public void Vastus()
+        {
+            Console.WriteLine($"S = {Pindala}");
+            Console.WriteLine($"P = {Perimeeter}");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Nelinurk
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            int b;
+
+            Console.WriteLine("Sisesta a");
+            a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Sisesta b");
+            b = int.Parse(Console.ReadLine());
+
+            Nelinurk N = new Nelinurk(a, b);
+            N.Vastus();
+            Console.ReadKey();
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
